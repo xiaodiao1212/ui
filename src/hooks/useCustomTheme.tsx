6a899e4db05ab2099ab3f51bad85711eac040952
xymzhs <xymzhs@gmail.com>
2021-06-30 18:52:07 +0800

    Fix useCustomTheme's bug

--- src/hooks/useCustomTheme.tsx
+++ src/hooks/useCustomTheme.tsx
@@ -1,6 +1,6 @@
 import { theme, Theme } from "../constants/theme";
-const useCustomTheme = (customTheme: {}) => {
-  return Object.assign(customTheme, theme) as Theme;
+const useCustomTheme = (customTheme: Partial<Theme>) => {
+  return Object.assign(theme, customTheme) as Theme;
 };
 
 export default useCustomTheme;
