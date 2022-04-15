dfd2835037c982e18592f866cd5dc93a696c58c5
trevorXu <xymzhs@gmail.com>
2022-04-15 15:41:46 +0800

    docs : change swiper docs

---
+++ DEV.md
@@ -0,0 +1,5 @@
+<!-- markdownlint-disable-next-line -->
+
+<h1 align="center">开发指南</h1>
+
+<div align="center">
--- example/src/Swiper.examples.tsx
+++ example/src/Swiper.examples.tsx
@@ -25,9 +25,14 @@ const SwiperExamples = () => {
         title='Default'
         desc='The Link component allows you to easily customize anchor elements with your theme colors and indicator
       styles.'>
-        <Left>
-          <Swiper items={[]}>no indicator</Swiper>
-        </Left>
+        <Swiper indicator={e=>{}}>
+          {[1, 2, 3, 4].map(v => (
+            <Swiper.Item>{1}</Swiper.Item>
+          ))}
+          <Swiper.IndicatorSlot>
+            {(v1,v2,3)=><div></div>}
+          </Swiper.IndicatorSlot>
+        </Swiper>
       </Example>
 
       <h1 style={{ marginBottom: '1em' }}>Props</h1>
